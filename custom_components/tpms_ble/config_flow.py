b3702a04a66855124efd35834ef47235e2e7641b
OliverJarisch <49845334+OliverJarisch@users.noreply.github.com>
2024-04-11 20:01:39 +0200

    Update config_flow.py

--- custom_components/tpms_ble/config_flow.py
+++ custom_components/tpms_ble/config_flow.py
@@ -86,7 +86,7 @@ class TPMSConfigFlow(ConfigFlow, domain=DOMAIN):
                 self._discovered_devices[address] = (
                     device.title or device.get_device_name() or discovery_info.name
                 )
-         _LOGGER.debug("Test adsaodj")
+        _LOGGER.debug("Test adsaodj")
         
         if not self._discovered_devices:
             return self.async_abort(reason="no_devices_found")
