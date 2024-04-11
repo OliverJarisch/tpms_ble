792ee5373db7541015122e0015e4ed9dd4f450fe
OliverJarisch <49845334+OliverJarisch@users.noreply.github.com>
2024-04-11 23:45:48 +0200

    Update parser.py

--- custom_components/tpms_ble/tpms_parser/parser.py
+++ custom_components/tpms_ble/tpms_parser/parser.py
@@ -8,7 +8,7 @@ from enum import Enum, auto
 
 from bluetooth_data_tools import short_address
 from bluetooth_sensor_state_data import BluetoothData
-from home_assistant_bluetooth import BluetoothServiceInfo
+from home_assistant_bluetooth import BluetoothServiceInfoBleak
 from sensor_state_data.enum import StrEnum
 
 _LOGGER = logging.getLogger(__name__)
@@ -32,7 +32,7 @@ TPMS_MANUFACTURER = 256 # indicator of the manufacturer Data set
 class TPMSBluetoothDeviceData(BluetoothData):
     """Data for TPMS BLE sensors."""
 
-    def _start_update(self, service_info: BluetoothServiceInfo) -> None:
+    def _start_update(self, service_info: BluetoothServiceInfoBleak) -> None:
         """Update from BLE advertisement data."""
         _LOGGER.debug("Parsing TPMS BLE advertisement data: %s", service_info)
         manufacturer_data = service_info.manufacturer_data
@@ -43,11 +43,11 @@ class TPMSBluetoothDeviceData(BluetoothData):
         mfr_data = manufacturer_data[TPMS_MANUFACTURER]
         self.set_device_manufacturer("TPMS")
 
-        self._process_mfr_data(address, mfr_data)
+        self._process_mfr_data(mfr_data)   #(address, mfr_data)
 
     def _process_mfr_data(
         self,
-        address: str,
+#        address: str,
         data: bytes,
     ) -> None:
         """Parser for TPMS sensors."""
@@ -61,10 +61,10 @@ class TPMSBluetoothDeviceData(BluetoothData):
         pressure = int.from_bytes(data[14:16], byteorder='big', signed=False) / 10000  # Druck (83 9c in Ihrem Beispiel)
     
 
-        name = f"TPMS {short_address(address)}"
-        self.set_device_type(name)
-        self.set_device_name(name)
-        self.set_title(name)
+#        name = f"TPMS {short_address(address)}"
+#        self.set_device_type(name)
+#        self.set_device_name(name)
+#        self.set_title(name)
 
         self.update_sensor(
             str(TPMSSensor.PRESSURE), None, pressure, None, "Pressure"
