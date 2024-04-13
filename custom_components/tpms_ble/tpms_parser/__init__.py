9af396110a4cb334b264a3915248a965e83418ce
OliverJarisch <Oliver.Jarisch23@gmail.com>
2024-04-13 15:29:02 +0200

    did a lot of stuff, hope it works

--- custom_components/tpms_ble/tpms_parser/__init__.py
+++ custom_components/tpms_ble/tpms_parser/__init__.py
@@ -1,34 +1,10 @@
 """Parser for TPMS BLE advertisements."""
 from __future__ import annotations
 
-from sensor_state_data import (
-    BinarySensorDeviceClass,
-    BinarySensorValue,
-    DeviceKey,
-    SensorDescription,
-    SensorDeviceClass,
-    SensorDeviceInfo,
-    SensorUpdate,
-    SensorValue,
-    Units,
-)
-
-from .parser import TPMSBinarySensor, TPMSBluetoothDeviceData, TPMSSensor
+from .parser import TPMSBluetoothDeviceData
 
 __version__ = "0.1.0"
 
 __all__ = [
-    "TPMSSensor",
-    "TPMSBinarySensor",
     "TPMSBluetoothDeviceData",
-    "BinarySensorDeviceClass",
-    "BinarySensorValue",
-    "SensorDescription",
-    "SensorDeviceInfo",
-    "DeviceKey",
-    "SensorUpdate",
-    "SensorDeviceClass",
-    "SensorDeviceInfo",
-    "SensorValue",
-    "Units",
 ]
